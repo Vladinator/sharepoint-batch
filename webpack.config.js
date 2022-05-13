134bb87e9f0838165b84adb5f74869eab2ec5ceb
Vladinator89 <vladix@gmail.com>
2022-05-14 00:18:06 +0200

    webpack works better for browsers

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,45 @@
 const path = require('node:path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     devtool: 'inline-source-map',
     entry: './test.ts',
     mode: 'development',
+    target: ['web', 'es5'],
     module: {
-        rules: [{
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-        }]
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
     },
     output: {
-        filename: 'build.js',
-        path: path.resolve(__dirname, 'build')
+        filename: 'build.min.js',
+        path: path.resolve(__dirname, 'build'),
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js']
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    ecma: 5,
+                    ie8: true,
+                    safari10: true,
+                    compress: {
+                        ecma: 5,
+                    },
+                    mangle: {
+                        safari10: true,
+                    },
+                },
+            }),
+        ],
     },
 };
