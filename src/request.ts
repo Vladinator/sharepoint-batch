d2040891d244d2d1ed733d3d9c0eb2cec488885f
Vladinator89 <vladix@gmail.com>
2022-05-14 02:05:15 +0200

    generic syntax will be the end of me

--- src/request.ts
+++ src/request.ts
@@ -1,33 +1,64 @@
-import { RequestOptions } from './types';
+import { CallbackProps, RequestOptions, RequestResponse } from './types';
 
-const SafeCall = (options: RequestOptions, prop: string, response: Response | undefined, ...args: any) => {
-    const value = options[prop] as Function;
+const SafeCall = (options: RequestOptions, prop: CallbackProps, response: RequestResponse, ...args: any) => {
+    const value = options[prop];
     if (typeof value === 'function')
-        value.call(options, options, response, ...args);
+        value.call(null, options, response, ...args);
 };
 
-export const Request = async (options: RequestOptions): Promise<Response | undefined> => {
+export const Request = async (options: RequestOptions): Promise<RequestResponse> => {
 
     return new Promise(async resolve => {
 
-        let response: Response | undefined;
-
+        let response: RequestResponse;
         SafeCall(options, 'before', response);
 
         try {
             response = await fetch(options.url, options as never);
-            SafeCall(options, 'progress', response);
             SafeCall(options, 'done', response);
-        } catch (ex) {
-            console.error(ex);
-            SafeCall(options, 'fail', response);
+        } catch (ex: any) {
+            SafeCall(options, 'fail', response, ex);
         }
 
-        SafeCall(options, 'always', response);
-        SafeCall(options, 'after', response);
-
+        SafeCall(options, 'finally', response);
         resolve(response);
 
     });
+
+};
+
+type ObjectToMethodMapRecord = {
+    type: any;
+    prop: 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text';
+};
+
+const ObjectToMethodMap: ObjectToMethodMapRecord[] = [
+    { type: ArrayBuffer, prop: 'arrayBuffer' },
+    { type: Blob, prop: 'blob' },
+    { type: FormData, prop: 'formData' },
+    { type: Object, prop: 'json' },
+    { type: String, prop: 'text' },
+];
+
+export const RequestResolve = async <T>(options: RequestOptions, type?: T): Promise<T | undefined> => {
+
+    const response = await Request(options);
+
+    if (!response)
+        return;
+
+    if (!type || type instanceof Response)
+        return response as never;
+
+    for (const map of ObjectToMethodMap) {
+        if (type instanceof map.type) {
+            try {
+                const result = await response[map.prop]();
+                return result as T;
+            } catch (ex: any) {
+                return;
+            }
+        }
+    }
 
 };
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,17 @@
 export type CallbackOptions = {
     before?: Function;
-    progress?: Function;
     done?: Function;
     fail?: Function;
-    always?: Function;
-    after?: Function;
+    finally?: Function;
 }
+
+export type CallbackProps = 'before' | 'done' | 'fail' | 'finally';
 
 export type RequestOptions = CallbackOptions & RequestInit & {
     method: 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE' | 'PATCH';
     url: string;
 }
+
+export type RequestResponse = Response | undefined;
+
+export type RequestReturnType = JSON | Object | String | Blob | ArrayBuffer | FormData | RequestResponse;
